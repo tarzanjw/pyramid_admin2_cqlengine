3128caecd52c0f8cd772c9a5119e5eb7389865f9
Tarzan <hoc3010@gmail.com>
2014-10-09 12:22:54 +0700

    first commit

---
+++ README.rst
@@ -0,0 +1,17 @@
+========================
+pyramid_admin2_cqlengine
+========================
+
+This package supports cqlengine database backend for pyramid_admin2
+
+
+Usage
+=====
+
+Just include it
+
+.. code:: python
+
+    config.include('pyramid_admin2_cqlengine')
+
+before you include `pyramid_admin2`
---
+++ setup.py
@@ -0,0 +1,31 @@
+import os
+
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+
+requires = [
+    'pyramid_admin2',
+    'cqlengine',
+    ]
+
+setup(name='pyramid_admin2_cqlengine',
+      version='0.1',
+      description='pyramid_admin2_cqlengine',
+      long_description=README,
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pyramid",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      author='Hoc .T Do',
+      author_email='hoc3010@gmail.com',
+      url='https://github.com/tarzanjw/pyramid_admin2_cqlengine',
+      keywords='web wsgi bfg pylons pyramid',
+      packages=['pyramid_admin2_cqlengine', ],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=requires,
+      )
